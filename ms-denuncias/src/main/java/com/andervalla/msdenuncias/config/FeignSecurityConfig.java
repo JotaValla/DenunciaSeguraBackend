c2224c93a703f21c131b2aff8c4eb6dd58848932
JotaValla <jimmy.valladares@epn.edu.ec>
2026-01-16 10:36:24 -0500

    feat: proteger denuncias y evidencias con jwt y propagar token feign

---
+++ ms-denuncias/src/main/java/com/andervalla/msdenuncias/config/FeignSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.andervalla.msdenuncias.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+@Configuration
+public class FeignSecurityConfig {
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor() {
+        return template -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication instanceof JwtAuthenticationToken jwtAuth) {
+                String token = jwtAuth.getToken().getTokenValue();
+                template.header(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+            }
+        };
+    }
+}
