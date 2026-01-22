3637fb122efa657f819cdf24ee97626157b5b2a0
JotaValla <jimmy.valladares@epn.edu.ec>
2026-01-21 23:04:28 -0500

    fix: servicio de auth para oauth2 con headers

---
+++ ms-auth/src/main/java/com/andervalla/msauth/config/ForwardedHeadersConfig.java
@@ -0,0 +1,16 @@
+package com.andervalla.msauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+public class ForwardedHeadersConfig {
+
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        filter.setRemoveOnly(false);
+        return filter;
+    }
+}
